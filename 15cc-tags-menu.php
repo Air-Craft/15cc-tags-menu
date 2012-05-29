985566f53b530536eadc5151fdb10339a22325f5
Hari Karam Singh <harikaram@regallygraceful.com>
2012-05-29 19:16:22 +0100

    v0.2 preliminary.  Custom tax's supported including auto-detect.  'Include only these tags' feature added

--- 15cc-tags-menu.php
+++ 15cc-tags-menu.php
@@ -3,7 +3,7 @@
  Plugin Name: 15CC Tag Menu
  Plugin URI: http://club15cc.com/
  Description: A customisable tag menu (not a cloud!) with active term highlighting and other features
- Version: 0.1
+ Version: 0.2
  Author: Hari Karam Singh
  Author URI: http://club15cc.com
  License: MIT
@@ -12,6 +12,9 @@
 /**
  * FEATURES:
  * - 15cc-hidden-tags plugin bridge!
+ * - Custom taxonomies selection
+ * - Auto-detection of custom tag taxonomies for custom post types
+ * - Tag specifed by name or slug, newline or comma separated and '#' for line commenting
  */
 class l5CC_Tag_Menu_Widget extends WP_Widget 
 {
@@ -29,23 +32,52 @@ class l5CC_Tag_Menu_Widget extends WP_Widget
     {
         // Defaults
         $instance['taxonomy'] = $instance['taxonomy'] ? $instance['taxonomy'] : 'post_tag';
+        $instance['orderby'] = $instance['orderby'] ? $instance['orderby'] : 'count';
+        $instance['order'] = $instance['order'] ? $instance['order'] : 'DESC';
+        
+        
         ?>
         <p><label for="<?php echo $this->get_field_id('title'); ?>"><?php _e('Title:') ?></label>
         <input type="text" class="widefat" id="<?php echo $this->get_field_id('title'); ?>" name="<?php echo $this->get_field_name('title'); ?>" value="<?php if (isset ( $instance['title'])) {echo esc_attr( $instance['title'] );} ?>" /></p>
         
+        <hr/>
+        <h3>Filtering</h3>
+        
         <p><label title="Minimum number of posts required for the tag to show." for="<?php echo $this->get_field_id('min_post_count'); ?>"><?php _e('Minimum Post Count:') ?></label>
-        <input type="text" class="" id="<?php echo $this->get_field_id('min_post_count'); ?>" name="<?php echo $this->get_field_name('min_post_count'); ?>" value="<?php if (isset ( $instance['min_post_count'])) {echo esc_attr( $instance['min_post_count'] );} ?>" /></p>
+        <input type="number" class="" id="<?php echo $this->get_field_id('min_post_count'); ?>" name="<?php echo $this->get_field_name('min_post_count'); ?>" value="<?php if (isset ( $instance['min_post_count'])) {echo esc_attr( $instance['min_post_count'] );} ?>" /></p>
         
-        <p><label title="Exclude the tags with the following comma seperated list. Names or slugs accepted." for="<?php echo $this->get_field_id('exclude_tags'); ?>"><?php _e('Excluded these tags:') ?></label>
-        <input type="text" class="widefat" id="<?php echo $this->get_field_id('exclude_tags'); ?>" name="<?php echo $this->get_field_name('exclude_tags'); ?>" value="<?php if (isset ( $instance['exclude_tags'])) {echo esc_attr( $instance['exclude_tags'] );} ?>" /></p>
+        <p><label title="Exclude the tags with the following comma/newline seperated list. Names or slugs accepted. Use '#' to mark a line as a comment." for="<?php echo $this->get_field_id('exclude_tags'); ?>"><?php _e('Excluded these tags:') ?></label>
+        <textarea class="widefat" id="<?php echo $this->get_field_id('exclude_tags'); ?>" name="<?php echo $this->get_field_name('exclude_tags'); ?>"><?php if (isset ( $instance['exclude_tags'])) {echo esc_attr( $instance['exclude_tags'] );} ?></textarea></p>
         
-        <p><label title="Include only these tags (provided they meet the min post count). Names or slugs accepted." for="<?php echo $this->get_field_id('include_tags'); ?>"><?php _e('Include only these tags:') ?></label>
-        <input type="text" class="widefat" id="<?php echo $this->get_field_id('include_tags'); ?>" name="<?php echo $this->get_field_name('include_tags'); ?>" value="<?php if (isset ( $instance['include_tags'])) {echo esc_attr( $instance['include_tags'] );} ?>" /></p>
+        <p><label title="Include only these tags (provided they meet the min post count). Names or slugs accepted. Use '#' to mark a line as a comment." for="<?php echo $this->get_field_id('include_tags'); ?>"><?php _e('Include only these tags:') ?></label>
+        <textarea class="widefat" id="<?php echo $this->get_field_id('include_tags'); ?>" name="<?php echo $this->get_field_name('include_tags'); ?>"><?php if (isset ( $instance['include_tags'])) {echo esc_attr( $instance['include_tags'] );} ?></textarea></p>
+        
+        <h3>Order By</h3>
+        
+        <select id="<?php echo $this->get_field_id('orderby'); ?>" name="<?php echo $this->get_field_name('orderby'); ?>">
+            <?php foreach (array(   'id'=>'ID',
+                                    'count' => 'Count',
+                                    'name' => 'Name',
+                                    'slug' => 'Slug',
+                                    'term_group' => 'Term Group',
+                                    'none' => 'None' ) as $value => $label): ?>
+                <option value="<?php esc_attr_e($value)?>" <?php echo $instance['orderby'] == $value ? 'selected="selected"' : ''?>><?php esc_html_e($label)?></option>
+            <?php endforeach; ?> 
+        </select> 
+
+        <select id="<?php echo $this->get_field_id('order'); ?>" name="<?php echo $this->get_field_name('order'); ?>">
+            <?php foreach (array(   'ASC'=>'Ascending',
+                                    'DESC' => 'Descending',
+                                 ) as $value => $label): ?>
+                <option value="<?php esc_attr_e($value)?>" <?php echo $instance['order'] == $value ? 'selected="selected"' : ''?>><?php esc_html_e($label)?></option>
+            <?php endforeach; ?>  
+        </select> 
         
         <hr/>
         <h3>Advanced</h3>
-        <p><label title="" for="<?php echo $this->get_field_id('taxonomy'); ?>"><?php _e('Taxonomy:') ?></label>
+        <p><label title="Choose the taxonomy to display.  Auto-detect looks for a 'tags' derivative taxonomy for the current (custom) post type." for="<?php echo $this->get_field_id('taxonomy'); ?>"><?php _e('Taxonomy:') ?></label>
         <select id="<?php echo $this->get_field_id('taxonomy'); ?>" name="<?php echo $this->get_field_name('taxonomy'); ?>">
+            <option value="TAXONOMY_AUTO_DETECT">(auto-detect)</option> 
             <?php 
             foreach (get_taxonomies(array('public' => true), 'objects') as $tax): ?>
                 <option value="<?php esc_attr_e($tax->name)?>" <?php echo $instance['taxonomy'] == $tax->name ? 'selected="selected"' : ''?>><?php esc_html_e($tax->labels->name)?></option>
@@ -57,8 +89,12 @@ class l5CC_Tag_Menu_Widget extends WP_Widget
     public function update($new_instance, $old_instance) 
     {
         $instance['title'] = strip_tags(stripslashes($new_instance['title']));
-        $instance['exclude_tags'] = strip_tags(stripslashes($new_instance['exclude_tags']));
         $instance['min_post_count'] = (int)$new_instance['min_post_count'];
+        $instance['exclude_tags'] = strip_tags(stripslashes($new_instance['exclude_tags']));
+        $instance['include_tags'] = strip_tags(stripslashes($new_instance['include_tags']));
+        $instance['taxonomy'] = strip_tags(stripslashes($new_instance['taxonomy']));
+        $instance['order'] = strip_tags(stripslashes($new_instance['order']));
+        $instance['orderby'] = strip_tags(stripslashes($new_instance['orderby']));
         return $instance;     
     }
     
@@ -66,6 +102,10 @@ class l5CC_Tag_Menu_Widget extends WP_Widget
     {
         extract($args);
         extract($instance);
+        
+        if ($taxonomy == 'TAXONOMY_AUTO_DETECT') {
+            $taxonomy = self::_autodetect_taxonomy();
+        }
         
         // Bridge with 15cc hidden tags plugin list if available
         if (defined('FIFTEENCC_HIDDEN_TAGS_PLUGIN')) {
@@ -76,12 +116,8 @@ class l5CC_Tag_Menu_Widget extends WP_Widget
         }
         
         // Convert to array and clean
-        $exclude_tags = split(',', $exclude_tags);
-        $exclude_tags = array_unique(array_filter(array_map('trim', $exclude_tags)));
-        
-        /*if ( !empty($instance['title']) )        $title = $instance['title'];
-        if ( !empty($instance['exclude_tags']) ) $exclude_tags = $instance['exclude_tags'];
-        */
+        $exclude_tags = self::_process_tags_list_string($exclude_tags);
+        $include_tags = self::_process_tags_list_string($include_tags);
        
         // Get the active tags list..
         // For tags page, its the tag we're looking at...
@@ -90,7 +126,7 @@ class l5CC_Tag_Menu_Widget extends WP_Widget
         if (is_tag()) {
             $active_tagnames = array(single_tag_title("", false)); 
         } elseif (is_single()) {
-            $tmp = get_the_tags();
+            $tmp = get_the_terms( 0, $taxonomy );
             if (!empty($tmp)) {
                 foreach ($tmp as $tag) {
                     $active_tagnames[] = $tag->name;   
@@ -98,8 +134,42 @@ class l5CC_Tag_Menu_Widget extends WP_Widget
             }
         }
         
-        // Get all the tags excluding specified 
-        $tags = $this->_get_the_tags_excluding($exclude_tags, $min_post_count);
+        // Get all the tags for the taxonomy, using the 'include' list if specified
+        // or querying the system if not
+        $tags = array();
+        if (count($include_tags)) {
+            foreach ($include_tags as $slug_or_name) {
+                if ($t = self::_get_term_for_taxonomy($slug_or_name, $taxonomy)) {
+                    $tags[] = $t;
+                }
+            }
+        } else {
+            
+            $tags = get_terms($taxonomy, array(
+                'orderby'       => $orderby,
+                'order'         => $order,
+                'hide_empty'    => FALSE    // let the min post count define this
+            ));
+        }
+
+        // Exclude specified tags and those w/o minimum post requirement
+        foreach ($tags as $idx => $t) {
+            // Match on either name or slug
+            if ( (in_array($t->name, $exclude_tags, true) ||
+                  in_array($t->slug, $exclude_tags, true)) ) {
+                    
+                unset($tags[$idx]);
+                
+            // Also remove terms which dont have the minimum post count
+            } elseif ($min_post_count && $t->count < $min_post_count) {
+                unset($tags[$idx]);
+            }
+        }
+        $tags = array_values(array_filter($tags));  // clean empties & re-index
+            
+        //
+        // WIDGET OUTPUT
+        //
         
         // don't output the widget if nothing
         if ( empty( $tags ) || is_wp_error( $tags ) )
@@ -127,36 +197,6 @@ class l5CC_Tag_Menu_Widget extends WP_Widget
     } //widget()   
     
     /**
-     * Returns list of available tags which aren't set to be hidden by the widget options
-     * @param   array  $tags_to_hide    Array of names or slugs of tags to exclude
-     */
-    protected function _get_the_tags_excluding($tags_to_hide, $min_post_count = NULL)
-    {
-        $terms = get_terms('post_tag', array(
-            'orderby'       => 'count',
-            'order'         => 'DESC',
-            'hide_empty'    => true
-        ));
-        
-        // Loop thru terms and exclude matches
-        // Match on either name or slug
-        foreach ($terms as $idx => $term) {
-            if ( (in_array($term->name, $tags_to_hide, true) ||
-                  in_array($term->slug, $tags_to_hide, true)) ) {
-                    
-                unset($terms[$idx]);
-                
-            // Also remove terms which dont have the minimum post count
-            } elseif ($min_post_count && $term->count < $min_post_count) {
-                unset($terms[$idx]);
-            }
-            
-        }
-        $terms = array_filter($terms);  // re-index
-        return $terms; 
-    }
-    
-    /**
      * Get the reduced (integer, 0..$modulus) checksum for a string. 
      * 
      * Allows consistent custom styling for things like tags.
@@ -165,6 +205,56 @@ class l5CC_Tag_Menu_Widget extends WP_Widget
     protected function _get_checksum_modulus($str, $modulus)
     {
         return abs(crc32(md5($str)) % $modulus) + 1;
+    }
+    
+    /**
+     * Uses the custom post type name to guess and check for the a custom taxonomy 'tag'
+     * 
+     * Plain WP posts return plain tags.  Also defaults to 'tag' if no custom tax is found.  
+     * Otherwise it looks for a taxonomy ending in a non-alphanumeric + 'tag', for ex. 'myposttype-tag'
+     */
+    protected static function _autodetect_taxonomy()
+    {
+        $post_type = get_post_type();
+        
+        // Just plain 'posts'
+        if ($post_type == 'post')
+            return 'post_tag';
+        
+        foreach (get_object_taxonomies($post_type) as $tax) {
+            if (preg_match('/[^a-zA-Z0-9]{1}tag$/', $tax)) {
+                return $tax;
+            }
+        }
+        
+        return 'post_tag';
+    }    
+    
+    /**
+     * Returns a list of tag strings (names or slugs) from an input string
+     * 
+     * Tags can be separated by newline or comma.  '#' marks the beginning of comment line
+     */
+    protected static function _process_tags_list_string($input)
+    {
+        // Split by newline and ','
+        $input = preg_replace('/#(.*?)$/m', '', $input);
+        $input = preg_split('/[\r\n,]+/', $input);
+        
+        // Trim and clean up any empty entries and duplicates and finally re-index
+        return array_values(array_unique(array_filter(array_map('trim', $input))));
+    }
+    
+    /**
+     * Checks whether a given term name or slug exists for a specified taxonomy.
+     * @return false if none exist
+     */
+    protected static function _get_term_for_taxonomy($slug_or_name, $taxonomy) 
+    {
+        $term = get_term_by('slug', $slug_or_name, $taxonomy);
+        if (!$term)    
+            $term = get_term_by('name', $slug_or_name, $taxonomy);
+        return $term;
     }
 }
 add_action('widgets_init', create_function('', "register_widget('l5CC_Tag_Menu_Widget');"));
